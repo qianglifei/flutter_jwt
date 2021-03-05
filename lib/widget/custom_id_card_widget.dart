68d793c82a6c60cca1e31da492c4658b93920f64
qianglifei <qianglifei99>
2021-03-05 17:31:28 +0800

    人口核查界面搭建

--- lib/widget/custom_id_card_widget.dart
+++ lib/widget/custom_id_card_widget.dart
@@ -1,4 +1,22 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 
+class CustomIDCard extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return CustomIDCardState();
+  }
+}
+
+class CustomIDCardState extends State<CustomIDCard> {
+  @override
+  Widget build(BuildContext context) {
+     return Container(
+       child: Text(""),
+     );
+  }
+}
